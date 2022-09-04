4f7a9c498603fc6c92980d5c138934e48c7edef4
ranyellhenrique <ranyellhenrique@gmail.com>
2022-09-03 22:13:21 -0300

    Update navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { Text, View, Image, TouchableOpacity, } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
-import menu from '../assets/menu.png';
+import { Text, TouchableOpacity, } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { nav, text } from '../styles';
 import { doLogout, isAuthenticated } from '../services/auth';
 import { UserContext } from '../context';
@@ -9,20 +8,10 @@ import { UserContext } from '../context';
 
 const NavBar: React.FC = () => {
 
-    const [show, setShow] = useState(false);
     const [authenticated, setAuthenticated] = useState(false);
     const navigation = useNavigation();
-    const route = useRoute();
     const { setState, state } = useContext(UserContext);
 
-
-    const navigate = (path: any) => {
-        if (path) {
-            setShow(false);
-            navigation.navigate(path);
-        }
-        setShow(false);
-    }
 
     const logged = async () => {
         const result = await isAuthenticated();
@@ -34,6 +23,10 @@ const NavBar: React.FC = () => {
         doLogout();
         navigation.navigate('Perfil', { screen: 'Login' });
         setState({ email: "", isLogado: false, perfil: "", id: 0 })
+    }
+
+    const login = () => {
+        navigation.navigate('Perfil', { screen: 'Login' });
     }
 
     useEffect(() => {
@@ -51,55 +44,12 @@ const NavBar: React.FC = () => {
                         <Text style={text.logoutText}>Sair</Text>
                     </TouchableOpacity>
                 ) :
-                    (
-                        <TouchableOpacity activeOpacity={0.5} style={nav.drawer} onPress={() => setShow(!show)}>
-                            <Image source={menu} />
-                            {
-                                show ? (
-                                    <View style={nav.options}>
-                                        <TouchableOpacity
-                                            style={nav.option}
-                                            onPress={() => navigate("Home")}
-                                        >
-                                            <Text
-                                                style={[
-                                                    nav.textOption,
-                                                    route.name === "Home" ? nav.textActive : null
-                                                ]}
-                                            >
-                                                Home
-                                            </Text>
-                                        </TouchableOpacity>
-                                        <TouchableOpacity
-                                            style={nav.option}
-                                            onPress={() => navigate("Ofertas")}
-                                        >
-                                            <Text
-                                                style={[
-                                                    nav.textOption,
-                                                    route.name === "Ofertas" ? nav.textActive : null
-                                                ]}
-                                            >
-                                                Ofertas
-                                            </Text>
-                                        </TouchableOpacity>
-                                        <TouchableOpacity
-                                            style={nav.option}
-                                            onPress={() => navigate("Login")}
-                                        >
-                                            <Text
-                                                style={[
-                                                    nav.textOption,
-                                                    route.name === "Login" ? nav.textActive : null
-                                                ]}>
-                                                Login
-                                            </Text>
-                                        </TouchableOpacity>
-
-                                    </View>) : null
-                            }
-                        </TouchableOpacity>
-                    )
+                    <TouchableOpacity
+                        onPress={() => login()}
+                        style={nav.logoutBtn}
+                    >
+                        <Text style={text.logoutText}>Entrar</Text>
+                    </TouchableOpacity>
             }
         </>
 
